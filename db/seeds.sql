39d7c90aaa05595d2951d32c2f8122d2f762c0e5
Lindsay Stephens <lindsay.stephens7@gmail.com>
2019-11-14 19:36:29 -0800

    updated schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,40 @@
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers_db
+    (burger_name)
+VALUES
+    ('The Hulk');
+INSERT INTO burgers_db
+    (burger_name)
+VALUES
+    ('Meat Tornado');
+INSERT INTO burgers_db
+    (burger_name)
+VALUES
+    ('Whopper');
+INSERT INTO burgers_db
+    (burger_name)
+VALUES
+    ('Double Stack');
+INSERT INTO burgers_db
+    (burger_name)
+VALUES
+    ('Hamburger');
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ('Fake Meat', true);
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ('Classic', true);
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ('Big Mac', true);
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ('Triple Stack', true);
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ('Big Kahuna', true);
